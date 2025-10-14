9c74765772c4cf6f0c293bf69f1eeb81396e2f2e
harshitha <harshitha@gmail.com>
2025-10-14 15:02:42 +0530

    only footer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Home/Navbar.jsx';
 import Body from './Home/Body.jsx';
 import Output from './Output.jsx';
-
+import Footer from './Home/Footer.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <Footer/>
       <Navbar /> {/* Navbar visible on all pages */}
       <Routes>
         <Route path="/" element={<Body />} />        {/* Home page */}
